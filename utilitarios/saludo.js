a4293073fcb21201d555774f5eb4bf9a731bc21b
juankavp2007 <juankavp2007@gmail.com>
2025-09-23 16:16:07 -0500

    Saludo nombre apellido

---
+++ utilitarios/saludo.js
@@ -0,0 +1,12 @@
+saludar = function(){
+    let nombre=recuperarTexto("txtNombre");
+    let apellido=recuperarTexto("txtApellido");
+}
+
+recuperarTexto = function (idComponente) {
+    let componente;
+    let valorIngresado;
+    componente = document.getElementById(idComponente);
+    valorIngresado = componente.value;
+    return valorIngresado;
+}
