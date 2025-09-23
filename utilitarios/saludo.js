372f79867ff44cddb63ba81f71da64bff1e901c8
juankavp2007 <juankavp2007@gmail.com>
2025-09-23 16:47:13 -0500

    recuperar int-float

--- utilitarios/saludo.js
+++ utilitarios/saludo.js
@@ -1,6 +1,8 @@
 saludar = function(){
     let nombre=recuperarTexto("txtNombre");
     let apellido=recuperarTexto("txtApellido");
+    let edad=recuperarInt("txtEdad");
+    let Estatura=recuperarFloat("txtEstatura");
 }
 
 recuperarTexto = function (idComponente) {
@@ -9,4 +11,16 @@ recuperarTexto = function (idComponente) {
     componente = document.getElementById(idComponente);
     valorIngresado = componente.value;
     return valorIngresado;
+}
+
+recuperarInt = function (idComponente) {
+    let valorCaja = recuperarTexto(idComponente);
+    let valorEntero = parseInt(valorCaja);
+    return valorEntero;
+}
+
+recuperarFloat = function (idComponente) {
+    let valorCaja = recuperarTexto(idComponente);
+    let valorFlotante = parseFloat(valorCaja);
+    return valorFlotante;
 }
