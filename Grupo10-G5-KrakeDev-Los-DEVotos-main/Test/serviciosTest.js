50d61a37254e7a9f627984b6a47bdc5502968f46
juankavp2007-creator <juankavp2007@gmail.com>
2025-09-25 17:16:05 -0500

    Delete Grupo10-G5-KrakeDev-Los-DEVotos-main/Test directory

--- Grupo10-G5-KrakeDev-Los-DEVotos-main/Test/serviciosTest.js
+++
@@ -1,6 +0,0 @@
-generarSaludo=function(nombre,apellido){
-    let saludoCadena= nombre+" "+apellido
-    return saludoCadena
-}
-
-// console.log(generarSaludo("Rosa","Navas"))
--- Grupo10-G5-KrakeDev-Los-DEVotos-main/Test/test.js
+++
@@ -1,7 +0,0 @@
-saludar = function(){
-    let nombre = recuperarTexto("txtNombre");
-    let apellido = recuperarTexto("txtApellido");
-    let resultado = generarSaludo(nombre,apellido);
-    console.log(resultado);
-}
-
