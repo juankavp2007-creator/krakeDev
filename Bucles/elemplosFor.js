a3e6b637b128ba8d7fe60584f6f2f211287c9d1d
juankavp2007 <juankavp2007@gmail.com>
2025-10-24 19:36:21 -0500

    se implemento la funcion Mostrar Inpares del reto 37

---
+++ Bucles/elemplosFor.js
@@ -0,0 +1,54 @@
+mostrarNumero=function(){
+    console.log("antes del for")
+    for(let i=0;i<4;i++){
+        console.log(i)
+    }
+
+    console.log("despues del for")
+}
+
+mostrarNumero2=function(){
+    console.log("antes del for")
+    for(let indice=0;indice<=5;indice++){
+        console.log(indice)
+    }
+
+    console.log("despues del for")
+}
+
+mostrarPares=function(){
+    console.log("antes del for")
+    for(let x=2;x<=10;x+=2){
+        console.log(x)
+    }
+
+    console.log("despues del for")
+
+}
+
+mostrarInverso=function(){
+    console.log("antes del for")
+    for(let y=0;y>-5;y--){
+        console.log(y)
+    }
+
+    console.log("despues del for")
+}
+jakearNasaPelis=function(){
+    console.log("antes del for")
+    for(let y=0;y<=100;y+=10){
+        console.log("lakeando Nasa "+y+"%")
+    }
+
+    console.log("despues del for")
+}
+
+mostrarInpares=function(){
+    console.log("antes del for")
+    for(let x=1;x<=21;x+=2){
+        console.log(x)
+    }
+
+    console.log("despues del for")
+
+}
